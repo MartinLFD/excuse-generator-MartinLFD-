c2741292aa92e8ee5c8ec18c404c8c8fbe96a4b4
MartinLFD <168610198+MartinLFD@users.noreply.github.com>
2024-06-14 21:36:13 -0400

    primer-generador-excusas

---
+++ app.js
@@ -0,0 +1,25 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+function excuses() {
+
+   let quien = who[Math.floor(Math.random() * 4)] 
+   let accion = action[Math.floor(Math.random() * 4)]
+   let que = what[Math.floor(Math.random() * 3)]
+   let cuando = when[Math.floor(Math.random() * 5)]  
+
+    const resultadoexcusa = (quien + " " + accion + " " + que + " " + cuando)
+    return resultadoexcusa;
+ }
+
+
+ console.log(excuses());  
+
+
+
+
+
+
